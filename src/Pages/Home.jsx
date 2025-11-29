30905a97e07eef9b36d9e586d36c1dc075340385
Sam-Install <samielDev25@outlook.com>
2025-11-29 13:14:08 -0800

    second

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,18 +5,22 @@ import Dedicated from '../Components/Dedicated'
 import FeaturedClasses from '../Components/FeaturedClasses'
 import Bmi from '../Components/Bmi'
 import Locations from '../Components/Locations'
+// Import Elfsight widget
+import { ElfsightWidget } from 'react-elfsight-widget'
 
 const Home = () => {
   return (
     <div>
+      <Hero/>
+      <Abouts/>
+      <Dedicated/>
+      <FeaturedClasses/>
+      <Bmi/>
+      <Locations/>
 
-<Hero/>
-<Abouts/>
-<Dedicated/>
-<FeaturedClasses/>
-<Bmi/>
-<Locations/>
-
+    
+<script src="https://elfsightcdn.com/platform.js" async></script>
+<div class="elfsight-app-b326d7a9-5d20-44ab-bd3f-fc6d0e84c123" data-elfsight-app-lazy></div>
     </div>
   )
 }
